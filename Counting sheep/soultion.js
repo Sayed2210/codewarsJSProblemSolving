eeefecc396fe40806c1d131ebcb01a96eb6a8ff3
Elsayed Hassan <eh600039@gmail.com>
2022-12-04 20:39:17 +0200

    i did it Rank:7 kyu

---
+++ Counting sheep/soultion.js
@@ -0,0 +1,21 @@
+/*Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined*/
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    let counter = 0
+    for(let i = 0; i < arrayOfSheep.length; i++) {
+      if(arrayOfSheep[i]) counter += 1;
+    }
+    return counter;
+}
