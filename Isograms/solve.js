7ea53f7aa078595ddd7ee6e5900f57f47850a72f
Elsayed Hassan <eh600039@gmail.com>
2023-01-27 23:17:49 +0200

    Convert a Boolean to a String/         Isograms/         Opposites Attract/

---
+++ Isograms/solve.js
@@ -0,0 +1,15 @@
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+function isIsogram(str){
+    //...
+    str = str.toLowerCase();
+    for(var i=0; i<str.length-1; i++){
+      for(var j=i+1; j<str.length; j++){
+        if(str[i]==str[j]) return false;
+      }
+    }
+    return true;
+  }
